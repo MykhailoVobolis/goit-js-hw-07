c6ac8f5aff93d8ed6fe2171285a7294d6f267fc7
MykhailoVobolis <mvobolis@gmail.com>
2024-01-23 00:10:26 +0200

    task-6 created

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,35 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const numInput = document.querySelector('input');
+const btnCreate = document.querySelector('[data-create]');
+const btnDestroy = document.querySelector('[data-destroy]');
+const colorItemBox = document.querySelector('#boxes');
+
+btnCreate.addEventListener('click', () => {
+  if (numInput.value >= 1 && numInput.value <= 100) {
+    function createBoxes(amount) {
+      let increaseValue = 30;
+      for (let i = 0; i < amount; i++) {
+        const item = document.createElement('div');
+        // item.classList.add('boxesitem');
+        item.style.backgroundColor = getRandomHexColor();
+        item.style.width = `${increaseValue}px`;
+        item.style.height = `${increaseValue}px`;
+        colorItemBox.append(item);
+        increaseValue += 10;
+      }
+      console.log(amount);
+    }
+    createBoxes(numInput.value);
+  }
+
+  console.log(colorItemBox.childNodes);
+
+  numInput.value = '';
+});
+
+// btnDestroy.addEventListener('click', event => {
+//   colorItemBox.childNodes.remove();
+// });
