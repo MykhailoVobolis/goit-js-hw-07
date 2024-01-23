c0b34d101ac35ae1509b0f89bc55fbfca768e672
MykhailoVobolis <mvobolis@gmail.com>
2024-01-24 00:34:05 +0200

    task-6 New ready

--- js/task-6 new.js
+++ js/task-6 new.js
@@ -4,6 +4,7 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
+//Функція створення елементів <div>
 function createBoxes(amount) {
   let increaseValue = 30;
   for (let i = 0; i < amount; i++) {
@@ -22,21 +23,25 @@ const btnCreate = document.querySelector('[data-create]');
 const btnDestroy = document.querySelector('[data-destroy]');
 const colorItemBox = document.querySelector('#boxes');
 
+// Додавання елементів <div> у контейнер при кліку на button Create
 btnCreate.addEventListener('click', () => {
-  //   if (colorItemBox.children.classList.contains('boxes-item') === true) {
-  //     console.log('hello!');
-  //   }
-
-  //   colorItemBox.childNodes.remove();
-
+  // Перевірка наявності елементів у контейнері та їх відалення
+  if (colorItemBox.childNodes.length !== 0) {
+    const deletEl = document.querySelectorAll('.boxes-item');
+    deletEl.forEach(element => {
+      element.remove();
+    });
+  }
+  //Первірка валідності введенного значення
   if (numInput.value >= 1 && numInput.value <= 100) {
     createBoxes();
     createBoxes(numInput.value);
   }
+  // Очіщення поля вводу значень
   numInput.value = '';
 
+  // Очіщення результату при кліку на button Destroy
   const deletEl = document.querySelectorAll('.boxes-item');
-  //   console.log(deletEl);
 
   btnDestroy.addEventListener('click', () => {
     deletEl.forEach(element => {
